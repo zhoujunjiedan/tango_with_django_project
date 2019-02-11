b0d8fb0c19210cbfe317db1686c0245c4a6aba4e
junjie zhou <2371033z@student.gla.ac.uk>
2019-02-11 18:15:29 +0000

    commit 15

--- rango/forms.py
+++ rango/forms.py
@@ -1,33 +1,54 @@
 from django import forms
-from rango.models import Page, Category
 from django.contrib.auth.models import User
-from rango.models import Category, Page, UserProfile
+from rango.models import Page, Category, UserProfile
 
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,help_text="Please enter the category name.")
+    name = forms.CharField(max_length=128,
+                           help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
-    class Meta: 
+
+    class Meta:
         model = Category
-        fields = ('name',) 
+        fields = ('name',)
+
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200,help_text="Please enter the URL of the page.")
+    title = forms.CharField(max_length=128,
+                            help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200,
+                         help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
 
     class Meta:
         model = Page
         exclude = ('category',)
 
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+
+            return cleaned_data
+
+
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,22 +14,27 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '($l#%93xgg)jjnba)w_9d36g))^h(_in1l&p(k4$du*hb$iu51'
+SECRET_KEY = '$e)u1b!gh$h)^_cc-otuob7dg(-$&zaiz^0paf^w8ek70r=2^j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-STATICFILES_DIRS = [STATIC_DIR, ]
+
 
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -40,11 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rango',
 ]
-PASSWORD_HASHERS = (
-'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-)
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':  [TEMPLATE_DIR,],
+        'DIRS': [TEMPLATE_DIR ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,11 +69,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media'
+                'django.template.context_processors.media',
             ],
         },
     },
 ]
+
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
@@ -109,6 +111,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -125,8 +133,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = MEDIA_DIR 
-MEDIA_URL = '/media/'
+
 LOGIN_URL = '/rango/login/'
